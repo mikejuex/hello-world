8a8bbcb827d8a808c94353969e5d5dfa4040a7a6
mikejuex <675589531@qq.com>
2017-07-06 12:02:42 +0800

    index.php
    第一次提交

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<?php 
+	
+	header("Content-Type:text/html;charset=gbk");
+
+	$db = new PDO("mysql:host=127.0.0.1;dbname=ceshi","root","root");
+
+	$sql = "select * from web_members";
+
+	$data = $db->query($sql);
+
+	$data = $data->fetchAll();
+
+	print_r($data);die;
+	// try {
+	//     $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+	//     foreach($dbh->query('SELECT * from FOO') as $row) {
+	//         print_r($row);
+	//     }
+	//     $dbh = null;
+	// } catch (PDOException $e) {
+	//     print "Error!: " . $e->getMessage() . "<br/>";
+	//     die();
+	// }
